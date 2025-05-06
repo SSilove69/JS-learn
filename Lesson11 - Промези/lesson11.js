ba21e013f2dea8f65b77b034756d8aa50e0bdf7a
DarkSector <Sas-414@yandex.ru>
2025-05-06 18:03:14 +0300

    new lessons

--- Lesson11 - Промези/lesson11.js
+++ Lesson11 - Промези/lesson11.js
@@ -2,46 +2,47 @@
 // 2. Написать собственный класс Gemer, в которой будет часы проведенные в играх, никнейм, вес и все выводиться в пользователя.
 // 2.1 классная работа в переписке с Юликом
 
-function  fetchData() {
+// Асинхронные функции
+function fetchData() {
     return new Promise((resolve) => {
         setTimeout(() => {
-        resolve("Данные успешно загружены");
-    }, 2000);
+            resolve("Данные успешно загружены");
+        }, 2000);
     });
 }
 
 fetchData().then(console.log);
 
 function getCatFact() {
-        return fetch('https://catfact.ninja/fact')
+    return fetch('https://catfact.ninja/fact')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Ошибка HTTP: ${response.status}`);
             }
             return response.json();
         })
-            .then(data => {
-            return data.fact;
-            });
+        .then(data => data.fact);
 }
 
 getCatFact()
-    .then(fact => console.log("Факт:", fact))
+    .then(fact => console.log("Факт:", fact));
 
-
+// Класс Gamer
 class Gamer {
     constructor(nickname, hoursPlayed, weight) {
-        this.nickname = nickname;      // Никнейм игрока
-        this.hoursPlayed = hoursPlayed; // Часы, проведенные в играх
-        this.weight = weight;          // Вес игрока (в кг)
-
-        displayInfo() {
-            return `
-        Никнейм: ${this.nickname}
-        Часов в играх: ${this.hoursPlayed} ч
-        Вес: ${this.weight} кг
-        `;
-        }
+        this.nickname = nickname;
+        this.hoursPlayed = hoursPlayed;
+        this.weight = weight;
     }
 
-    const player1 = new Gamer("ProGamer2000", 1500, 75);
+    displayInfo() {
+        return `
+            Никнейм: ${this.nickname}
+            Часов в играх: ${this.hoursPlayed} ч
+            Вес: ${this.weight} кг
+        `;
+    }
+}
+
+const player1 = new Gamer("ProGamer2000", 1500, 75);
+console.log(player1.displayInfo());
---
+++ Lesson12/lesson12 асинхронность.js
@@ -0,0 +1,15 @@
+//Асинхроность
+console.log("Старт");
+
+setTimeout(() => {
+    console.log("Это сообщение появится через 2 секунды");
+}, 2000);
+
+console.log("Конец");
+
+console.log("Текст")
+
+setTimeout( () => {
+    console.log("Hello, World!");
+}, 3000);
+
